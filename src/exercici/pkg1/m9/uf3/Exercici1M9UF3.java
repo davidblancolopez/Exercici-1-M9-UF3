72157d0b25f184a7dd2bf8f6d908f9341bc1b796
davidblancolopez <daviddbl94@gmail.com>
2017-01-25 19:36:59 +0100

    CAMBIOS

---
+++ src/exercici/pkg1/m9/uf3/Exercici1M9UF3.java
@@ -0,0 +1,23 @@
+
+package exercici.pkg1.m9.uf3;
+
+import java.net.NetworkInterface;
+
+
+public class Exercici1M9UF3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try{
+            Enumeracion<NetworkInterface> en1 = NetworkInterface.getNetworkInterfaces();
+            while(en1.hasMoreElements()){
+                NetworkInterface ni = 
+            }
+        }catch(Exception e){
+            
+        }
+    }
+    
+}
