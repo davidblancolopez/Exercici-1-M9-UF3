1c32d17ca88c4efe70c287e0d6630159a8f0dd4f
davidblancolopez <daviddbl94@gmail.com>
2017-02-01 18:42:00 +0100

    CAMBIOS

--- src/exercici/pkg1/m9/uf3/Exercici1M9UF3.java
+++ src/exercici/pkg1/m9/uf3/Exercici1M9UF3.java
@@ -32,6 +32,55 @@ public class Exercici1M9UF3 {
         } catch(SocketException e) {
             System.out.println(e);
         }
+        /*******************************************************************************************************************
+         * *****************************************************************************************************************
+         * *****************************************************************************************************************
+         * *****************************************************************************************************************
+         */
+        
+        System.out.println("**************************************************************************************************"
+                + "************************************************************************************************************");
+        
+        InetAddress ip;
+        try {
+            ip = InetAddress.getLocalHost();
+
+            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+            
+            //Recolleix la MAC pero esta en un format que no es correcte perque no mostrara la informació
+            //com cal.
+            byte[] mac = network.getHardwareAddress();
+
+            System.out.print("Direcció MAC : ");
+
+            //Es crea una cadena on anirem posant la direcció MAC donant el format correcte a l'array mac. 
+            StringBuilder cadenaDireccioMAC = new StringBuilder();
+            
+            //El bucle va afegint a la cadena la informació que recorre en l'array mac, aplica format amb "%02X%s"
+            //i afegeix el '-' per a separar els numeros.
+            for (int i = 0; i < mac.length; i++) {
+                cadenaDireccioMAC.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+            }
+            //S'imprimeix la cadena amb la MAC formatada.
+            System.out.println(cadenaDireccioMAC.toString());
+
+        } catch (SocketException e) {
+            System.out.println(e);
+        }
+        
+        
+        
+        /*******************************************************************************************************************
+         * *****************************************************************************************************************
+         * *****************************************************************************************************************
+         * *****************************************************************************************************************
+         */
+        
+        System.out.println("**************************************************************************************************"
+                + "************************************************************************************************************");
+        
+        
+        
     }
     
 }
